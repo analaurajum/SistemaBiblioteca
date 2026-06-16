e5132c43237cee7ffac57663223744fe18371b35
allancalabria <allan.calabria@aluno.farroupilha.ifrs.edu.br>
2026-06-16 20:21:48 -0300

    Ajuste na conexão do banco de dados com o sistema
    Agora 100% funcional
    (Talvez precise baixar o Connectar e adionar no Intellij
    Link de download: https://dev.mysql.com/downloads/file/?id=552110)

--- src/main/java/dao/ConexaoDB.java
+++ src/main/java/dao/ConexaoDB.java
@@ -1,20 +1,26 @@
-package dao;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexaoDB {
-
-    private static final String url = "jdbc:mysql://localhost:3306/sistema_biblioteca";
-    private static final String usuario = "root";
-    private static final String senha = "root";
+    // Configurações de acesso
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "";
 
     public static Connection conectar() {
         try {
-            return DriverManager.getConnection(url, usuario, senha);
-        } catch (Exception e) {
-            System.out.println("Erro na conexão: " + e.getMessage());
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/biblioteca", USUARIO, SENHA);
+
+            System.out.println("Conexão realizada com sucesso!");
+            return conn;
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
             return null;
         }
+    }
+    public static void main(String[] args) {
+        conectar();
     }
 }
